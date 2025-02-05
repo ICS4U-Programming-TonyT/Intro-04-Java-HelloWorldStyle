8e0476f220f8db51d84808f8094fd224cf2718cd
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-05 16:02:11 +0000

    Added HelloWorldStyle.java

---
+++ HelloWorldStyle.java
@@ -0,0 +1,23 @@
+/**
+
+* The HelloWorld program implements an application that
+
+* simply displays "Hello World!" to the standard output.
+
+*
+
+* @author  Tony Tran
+
+* @version 1.0
+
+* @since   2025-02-05
+
+*/
+
+
+public class HelloWorldStyle {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+}
